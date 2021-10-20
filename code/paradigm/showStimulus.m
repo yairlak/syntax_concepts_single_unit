e158abceb40d530d9ff9be48f40a1b0834eb60ec
christonikos <christonik@gmail.com>
2021-10-20 18:14:16 +0200

    YL: small fix visual presentation

--- code/paradigm/showStimulus.m
+++ code/paradigm/showStimulus.m
@@ -9,11 +9,11 @@ for word = words
     DrawFormattedText(PTBparams.w, word{1}, 'center', 'center', [1, 1, 1]);
     text_onset = Screen('Flip', PTBparams.w); % Word ON
     ttlLog = ttl(sprintf('Word ON: %i, %s', word_cnt, word{1}),ttlLog);
-    WaitSecs('UntilTime', PTBparams.word_onset_duration);
+    WaitSecs(PTBparams.word_onset_duration);
     % TEXT OFF
     text_offset = Screen('Flip', PTBparams.w); % Word OFF
     ttlLog = ttl(sprintf('Word OFF: %i, %s', word_cnt, word{1}),ttlLog);  
-    WaitSecs('UntilTime', PTBparams.word_offset_duration);
+    WaitSecs(PTBparams.word_offset_duration);
 end % word
 
 
